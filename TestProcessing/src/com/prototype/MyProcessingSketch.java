26cd10b346546a98ffb9da21fc26ddd6a62548ac
Tony Ren <chairmanmeow50@gmail.com>
2013-04-06 20:19:59 -0400

    Initial commit with some sample code

---
+++ TestProcessing/src/com/prototype/MyProcessingSketch.java
@@ -0,0 +1,17 @@
+package com.prototype;
+
+import processing.core.*;
+
+public class MyProcessingSketch extends PApplet{
+	public void setup() {
+		size(200,200);
+		background(0);
+	}
+	
+	public void draw() {
+		stroke(255);
+		if (mousePressed) {
+			line(mouseX,mouseY,pmouseX,pmouseY);
+		}
+	}
+}
