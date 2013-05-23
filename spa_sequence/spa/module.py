bcab1408838c0858981ca596a817cbdadf7cd1da
Mike Chong <mcchong@uwaterloo.ca>
2013-05-23 13:29:06 -0400

    Demo Script from Terry
    Adding script from Terry for LFP demo data.

---
+++ spa_sequence/spa/module.py
@@ -0,0 +1,17 @@
+import inspect
+
+class Module:
+    def __init__(self, **params):
+        self.params=params
+    
+    def init(self):
+        pass
+        
+    def connect(self):
+        pass    
+        
+    def complete(self):
+        pass    
+        
+            
+
---
+++ spa_sequence/spa_sequence.py
@@ -0,0 +1,36 @@
+import spa
+import nengo.nef_theano as nef
+
+class Rules:
+    def A(state='A'):
+        effect(state='B')
+    def B(state='B'):
+        effect(state='C')
+    def C(state='C'):
+        effect(state='D')
+    def D(state='D'):
+        effect(state='E')
+    def E(state='E'):
+        effect(state='A')
+    
+class Sequence(spa.SPA):
+    dimensions = 8
+    verbose = True
+    
+    state = spa.Buffer()
+    BG = spa.BasalGanglia(Rules)
+    thal = spa.Thalamus(BG)
+    
+    input = spa.Input(0.1,state='D')
+
+net = nef.Network('Sequence', seed=1)
+seq = Sequence(net)
+
+pThal = net.make_probe('thal.rule', dt_sample=0.001)
+pGPi = net.make_probe('BG.GPi', dt_sample=0.001, data_type='spikes')
+pState = net.make_probe('state.buffer', dt_sample=0.001, data_type='spikes')
+
+net.run(1)
+
+net.write_data_to_hdf5('sequence.hd5')
+
