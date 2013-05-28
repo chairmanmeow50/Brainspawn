07bf6d2cf7c487d2e46bea3d45b69b1f93ece518
Mike Chong <mcchong@uwaterloo.ca>
2013-05-28 15:56:44 -0400

    Animated Spectrogram from spa_sequence demo

--- spa_sequence/spa_sequence.py
+++ spa_sequence/spa_sequence.py
@@ -1,5 +1,9 @@
 import spa
 import nengo.nef_theano as nef
+import numpy as np
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 class Rules:
     def A(state='A'):
@@ -12,15 +16,15 @@ class Rules:
         effect(state='E')
     def E(state='E'):
         effect(state='A')
-    
+
 class Sequence(spa.SPA):
     dimensions = 8
     verbose = True
-    
+
     state = spa.Buffer()
     BG = spa.BasalGanglia(Rules)
     thal = spa.Thalamus(BG)
-    
+
     input = spa.Input(0.1,state='D')
 
 net = nef.Network('Sequence', seed=1)
@@ -32,5 +36,36 @@ pState = net.make_probe('state.buffer', dt_sample=0.001, data_type='spikes')
 
 net.run(1)
 
-net.write_data_to_hdf5('sequence.hd5')
+dt = 0.001
+t = np.arange(0.0, net.run_time, dt)
+x = pThal.get_data()[:,0]  # the signal
 
+fig = plt.figure()
+
+ax1 = plt.subplot(211)
+plt.plot(t, x)
+
+# Pxx is the segments x freqs array of instantaneous power
+# freqs is the frequency vector
+# bins are the centers of the time bins in which the power is computed
+# im is the matplotlib.image.AxesImage instance
+plt.subplot(212, sharex=ax1)
+NFFT = 1024         # the length of the windowing segments
+Fs = int(1.0 / dt)  # the sampling frequency
+Pxx, freqs, bins, im = plt.specgram(x, NFFT=NFFT, Fs=Fs, noverlap=900, cmap=cm.gist_heat)
+
+def animate(i):
+	net.run(.01)
+
+	x = pThal.get_data()[:,0]  # the signal
+	t = np.linspace(0.0, net.run_time, x.size)
+	plt.subplot(211)
+	plt.plot(t, x)
+	plt.subplot(212, sharex=ax1)
+	NFFT = 1024         # the length of the windowing segments
+	Fs = int(1.0 / dt)  # the sampling frequency
+	return plt.specgram(x, NFFT=NFFT, Fs=Fs, noverlap=900, cmap=cm.gist_heat)
+
+ani = animation.FuncAnimation(fig, animate, interval=10)
+
+plt.show()
