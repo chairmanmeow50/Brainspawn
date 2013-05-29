78646c84d4b0b33f7e455fffd96e2cde1f8270b0
Andrew Tinits <amtinits@uwaterloo.ca>
2013-05-29 16:26:59 -0400

    Updated spa_sequence demo

--- spa_sequence/spa_sequence.py
+++ spa_sequence/spa_sequence.py
@@ -1,9 +1,10 @@
 import spa
 import nengo.nef_theano as nef
-import numpy as np
+
+import matplotlib.animation as animation
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+import numpy as np
 
 class Rules:
     def A(state='A'):
@@ -25,47 +26,34 @@ class Sequence(spa.SPA):
     BG = spa.BasalGanglia(Rules)
     thal = spa.Thalamus(BG)
 
-    input = spa.Input(0.1,state='D')
+    input = spa.Input(0.1, state='D')
 
 net = nef.Network('Sequence', seed=1)
 seq = Sequence(net)
 
 pThal = net.make_probe('thal.rule', dt_sample=0.001)
-pGPi = net.make_probe('BG.GPi', dt_sample=0.001, data_type='spikes')
-pState = net.make_probe('state.buffer', dt_sample=0.001, data_type='spikes')
+# pGPi = net.make_probe('BG.GPi', dt_sample=0.001, data_type='spikes')
+# pState = net.make_probe('state.buffer', dt_sample=0.001, data_type='spikes')
 
-net.run(1)
-
-dt = 0.001
-t = np.arange(0.0, net.run_time, dt)
-x = pThal.get_data()[:,0]  # the signal
+# net.run(1)
+# net.write_data_to_hdf5('sequence.hd5')
 
 fig = plt.figure()
-
 ax1 = plt.subplot(211)
-plt.plot(t, x)
-
-# Pxx is the segments x freqs array of instantaneous power
-# freqs is the frequency vector
-# bins are the centers of the time bins in which the power is computed
-# im is the matplotlib.image.AxesImage instance
-plt.subplot(212, sharex=ax1)
-NFFT = 1024         # the length of the windowing segments
-Fs = int(1.0 / dt)  # the sampling frequency
-Pxx, freqs, bins, im = plt.specgram(x, NFFT=NFFT, Fs=Fs, noverlap=900, cmap=cm.gist_heat)
+line, = ax1.plot([], [])
+ax2 = plt.subplot(212, sharex=ax1)
+Fs = 1.0 / pThal.dt_sample  # the sampling frequency
 
 def animate(i):
-	net.run(.01)
+    # run the simulator for another step
+    net.run(0.005)
 
-	x = pThal.get_data()[:,0]  # the signal
-	t = np.linspace(0.0, net.run_time, x.size)
-	plt.subplot(211)
-	plt.plot(t, x)
-	plt.subplot(212, sharex=ax1)
-	NFFT = 1024         # the length of the windowing segments
-	Fs = int(1.0 / dt)  # the sampling frequency
-	return plt.specgram(x, NFFT=NFFT, Fs=Fs, noverlap=900, cmap=cm.gist_heat)
+    # plot the output
+    x = pThal.get_data()[:,0]  # the signal
+    t = np.linspace(0.0, net.run_time, x.size)
+    line.set_data(t, x)
+    Pxx, freqs, bins, im = ax2.specgram(x, Fs=Fs, cmap=cm.gist_heat)
+    return line, im
 
-ani = animation.FuncAnimation(fig, animate, interval=10)
-
+ani = animation.FuncAnimation(fig, animate, interval=30, blit=True)
 plt.show()
