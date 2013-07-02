3a04f1ca7170469f798ab3d248852c8757e9ce8c
Tony Ren <chairmanmeow50@gmail.com>
2013-07-01 22:48:18 -0400

    added ax1 to self in spectrogram

--- src/spectrogram.py
+++ src/spectrogram.py
@@ -12,9 +12,9 @@ class Spectrogram(object):
         net.run(0.001)
 
         self.fig = plt.figure()
-        ax1 = plt.subplot(211)
-        self.line, = ax1.plot([], [])
-        self.ax2 = plt.subplot(212, sharex=ax1)
+        self.ax1 = plt.subplot(211)
+        self.line, = self.ax1.plot([], [])
+        self.ax2 = plt.subplot(212, sharex=self.ax1)
         self.Fs = 1.0 / pThal.dt_sample  # the sampling frequency
 
     def start_animate(self):
@@ -29,13 +29,31 @@ class Spectrogram(object):
         net.run(0.005)
 
         # plot the output
-        x = pThal.get_data()[:,0]  # the signal
-        t = np.linspace(0.0, net.run_time, x.size)
+        # 0 refers to the 0th signal, there are 5 signals actually
+        
+        x = pThal.get_data()[:,0]
+        print x
+        #xcropped=x[x.size-100:x.size,0]  # the signal
+        t = np.linspace(0, net.run_time, x.size)
+        print t
+        if (x.size > 100):
+            print "x.size:" + str(x.size)
+            x = x[x.size-100:x.size-1]
+            t = t[t.size-100:t.size-1]
+        self.line, = self.ax1.plot(t, x)
+        self.ax1.set_xlim(t[0], t[t.size-1])
         self.line.set_data(t, x)
+        
         Pxx, freqs, bins, im = self.ax2.specgram(x, Fs=self.Fs, cmap=cm.gist_heat)
         return self.line, im
 
-if __name__ == "__main__":
+def main():
     spec = Spectrogram()
     spec.start_animate()
+    #while (True): 
+    #    net.run(0.005)
+    
     plt.show()
+        
+if __name__ == "__main__":
+    main()
