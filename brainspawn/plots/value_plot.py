e8ad8485cc4bf91d5d20c8d6f3c7e7f639cdf2e7
Mike Chong <mcchong@uwaterloo.ca>
2014-03-13 02:42:02 -0400

    added periods to value_plot docstrings

--- brainspawn/plots/value_plot.py
+++ brainspawn/plots/value_plot.py
@@ -3,21 +3,21 @@ from plots.plot import Plot, registered_plot
 
 @registered_plot
 class ValuePlot(Plot):
-    """Graph for plotting values over time
+    """Graph for plotting values over time.
     """
 
     def __init__(self, main_controller, nengo_obj, capability):
         """
-        Set up the plot, and axis labels and limits
+        Set up the plot, and axis labels and limits.
 
         Args:
             main_controller (VisualizerController): The top-level controller
-            of the visualizer
-            nengo_obj (Nengo): The nengo object this plot is visualizing
+            of the visualizer.
+            nengo_obj (Nengo): The nengo object this plot is visualizing.
             capability (Capability): The capability of the object that this graph
-            is visualizing
+            is visualizing.
 
-        Note the call to super constructor
+        Note the call to super constructor.
         """
         super(ValuePlot, self).__init__(main_controller, nengo_obj, capability)
 
@@ -29,35 +29,35 @@ class ValuePlot(Plot):
 
     @staticmethod
     def plot_name():
-        """ What we call the plot
+        """ What we call the plot.
         (Used when choosing plot from dropdown menu)
 
         Returns:
-            string. The plot name
+            string. The plot name.
         """
         return "Value Plot"
 
     @staticmethod
     def supports_cap(cap):
-        """ Return true if this plot supports the given capability
+        """ Return true if this plot supports the given capability.
 
         Args:
-            capability (Capablility): The capability to check for plotability
+            capability (Capablility): The capability to check for plotability.
 
         Returns:
-            bool. True if this plot supports the given capability
+            bool. True if this plot supports the given capability.
         """
         return cap.name in ['voltages', 'output']
 
     def update(self, start_step, step_size, data):
-        """ Callback function passed to observer nodes
+        """ Callback function passed to observer nodes.
 
-        Update x data for each line in graph, and update axis limits as needed
+        Update x data for each line in graph, and update axis limits as needed.
 
         Args:
-            start_step (int): The initial step of the given data
-            step_size (int): The time, in simulated seconds, one step represents
-            data (int): The data from the simulator to plot
+            start_step (int): The initial step of the given data.
+            step_size (int): The time, in simulated seconds, one step represents.
+            data (int): The data from the simulator to plot.
         """
         start_time = start_step*step_size
         end_time = (start_step + data.shape[0])*step_size
