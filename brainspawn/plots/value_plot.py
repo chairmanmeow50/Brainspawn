88738473c4ab67dad80e0f3b0430458500812338
Mike Chong <mcchong@uwaterloo.ca>
2014-03-13 03:52:35 -0400

    better axis updating for valueplot

--- brainspawn/plots/value_plot.py
+++ brainspawn/plots/value_plot.py
@@ -8,7 +8,7 @@ class ValuePlot(Plot):
 
     def __init__(self, main_controller, nengo_obj, capability):
         """
-        Set up the plot, and axis labels and limits.
+        Set up the plot, and axis labels.
 
         Args:
             main_controller (VisualizerController): The top-level controller
@@ -24,8 +24,6 @@ class ValuePlot(Plot):
         self.lines = self.axes.plot([], np.empty((0, self.dimensions)))
         self.axes.set_ylabel(self.config['DATA'])
         self.axes.set_xlabel('time')
-        self.axes.set_ylim([0, 1])
-        self.axes.set_xlim([0, 1])
 
     @staticmethod
     def plot_name():
@@ -52,7 +50,7 @@ class ValuePlot(Plot):
     def update(self, start_step, step_size, data):
         """ Callback function passed to observer nodes.
 
-        Update x data for each line in graph, and update axis limits as needed.
+        Update x data for each line in graph, and autoscale axis limits as needed
 
         Args:
             start_step (int): The initial step of the given data.
@@ -68,13 +66,6 @@ class ValuePlot(Plot):
             line.set_xdata(t)
             line.set_ydata(data[:,idx:idx+1])
 
-        if end_time > 1 and len(t) > 1:
-            self.axes.set_xlim([t[0], t[-1]])
-        else:
-            self.axes.set_xlim([0, 1])
+        self.axes.relim()
+        self.axes.autoscale_view()
 
-        if len(data) > 0:
-            data_max = np.amax(data)
-            data_min = np.amin(data)
-            if data_max != data_min:
-                self.axes.set_ylim([min(data_min, 0), max(data_max, 1)])
