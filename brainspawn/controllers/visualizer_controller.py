b9e3e66b480bd8530452eab472882f56ff8746f2
Tony Ren <chairmanmeow50@gmail.com>
2014-04-19 00:29:18 -0400

    documented visualizer_controller.py

--- brainspawn/controllers/visualizer_controller.py
+++ brainspawn/controllers/visualizer_controller.py
@@ -1,8 +1,3 @@
-#
-"""
-Main controller for visualizer
-"""
-
 import os
 import imp
 import gtk
@@ -19,10 +14,15 @@ from plots.base_plot import REGISTERED_PLOTS
 
 class VisualizerController(object):
     """
-    A controller
+    Controller class for visualizer
+    Handles adding, removing plots, loading, saving of models and layouts
     """
 
     def __init__(self, sim_manager, model_file_name=None):
+        """ Initializes simulator manager, and lists for plots
+        Instantiates main frame as well
+        Then if a model file name was provided,
+        load that model file name """
         self.sim_manager = sim_manager
         self.network_view = NetworkView(self)
         self.dt = settings.SIMULATOR_DEFAULT_DELTA_TIME
@@ -41,7 +41,12 @@ class VisualizerController(object):
             self.load_model_from_filename(model_file_name, load_layout=True)
 
     def plots_for_object(self, obj):
-        """ Returns a list of plots available for this object
+        """ Gets list of capabilities of nodes in plot object
+        For each capability and for each registered plot
+        if the plot supports that type of capability,
+        append to list of available plots
+        
+        Returns the list of plots available for this object
         """
         supported_plots = []
         node_caps = self.sim_manager.get_caps_for_obj(obj)
@@ -53,7 +58,11 @@ class VisualizerController(object):
 
     def add_plot_for_obj(self, plt, obj, cap, config=None, position=None, 
                          size=None):
-        """ Callback for menu item
+        """ Instantiates plot object, set its config values if
+        config is provided, then appends to list of plots
+        Calls the connect_to_obj method to set up simulator manager
+        and plot object. Then finally calls show_plot to set up 
+        plot in visualizer frame
         """
         plot = plt(self, obj, cap)
         plot.set_config_values(config)
@@ -62,11 +71,19 @@ class VisualizerController(object):
         self.main_frame.show_plot(plot, False, position, size)
 
     def remove_plot_for_obj(self, plot, obj, cap):
+        """ Disconnects object from simulator manager,
+        calls remove method on plot to remove from visualizer
+        and list of plots
+        """
         self.sim_manager.disconnect_from_obj(obj, cap, plot.update)
         self.plots.remove(plot)
         self.main_frame.remove_plot(plot)
 
     def on_quit(self):
+        """ Event handler for when program quits
+        saves a layout file in json format of data from
+        get_layout_dict(), which contains layout data
+        """
         if self._loaded_model_file:
             filename = os.path.splitext(self._loaded_model_file)[0] + \
                 settings.LAYOUT_FILE_EXTENSION
@@ -74,6 +91,8 @@ class VisualizerController(object):
                 json.dump(self.get_layout_dict(), f)
 
     def on_open_model(self, widget):
+        """ Event handler for when open model menu item is clicked
+        """
         filename = self.file_open(ext=settings.PYTHON_FILE_EXTENSION, 
                                   ext_name=settings.PYTHON_FILE_EXTENSION_NAME)
         if not filename:
@@ -81,7 +100,11 @@ class VisualizerController(object):
         self.load_model_from_filename(filename, load_layout=True)
 
     def restore_layout_dict(self, dct, load_model=False):
-        """Restores layout from dict
+        """ Given data from layout dictionary, load model file
+        Loop through all plot objects in layout dictionary,
+        add the plot objects, then calls the restore method
+        on network view
+        
         Throws:
             ValueError - dct could not be loaded
         """
@@ -125,6 +148,11 @@ class VisualizerController(object):
         self.network_view.restore_layout(net_dict['network_layout'])
 
     def get_layout_dict(self):
+        """ Saves plot data, layout data
+        and network layout data into a json data
+        
+        Returns json data object
+        """
         layout_dict = {}
         # Save model file
         layout_dict['model'] = self._loaded_model_file
@@ -149,18 +177,22 @@ class VisualizerController(object):
         return {'layout': layout_dict}
 
     def get_uid_for_nengo(self, nengo_obj):
-        """Gets a consistent uid for the given nengo object
+        """ Returns a consistent unique id for the given nengo object
         """
         # Let's just use the network view's method right now, 
         # since that seems to be working great
         return self.network_view.get_name_from_obj(nengo_obj)
 
     def get_nengo_for_uid(self, uid):
-        """Gets a nengo object for a given uid
+        """ Returns a nengo object for a given uid
         """
         return self.network_view.get_obj_from_name(uid)
 
     def load_model_from_filename(self, filename, load_layout=False):
+        """ Tries to load model from a filename
+        If successful, load a layout file with the associated model
+        file
+        """
         mod_name, file_ext = os.path.splitext(os.path.basename(filename))
         try:
             module = imp.load_source(mod_name, filename)
@@ -194,6 +226,8 @@ class VisualizerController(object):
                     self.network_view.init_default_config()
 
     def show_err_dialog(self, message, secondary):
+        """ Show an error message in a dialog
+        """
         dialog = Gtk.MessageDialog(self.main_frame.window, 0, 
                                    Gtk.MessageType.INFO,
             Gtk.ButtonsType.OK, message)
@@ -226,6 +260,9 @@ class VisualizerController(object):
         nengo_obj.id = obj_id.hexdigest()
 
     def load_model(self, model):
+        """ Removes current models, and resets current visualizer state
+        Loads new model in network view and simulator manager
+        """
         copy_plots = self.plots[:]
         for plt in copy_plots:
             plt.remove_plot(None, None)
@@ -242,6 +279,9 @@ class VisualizerController(object):
         self.sim_manager.load_new_model(model, self.dt) 
 
     def load_plots(self):
+        """ Loads all plot files by looking for python file extension
+        in plot directory
+        """
         plots_dir = os.path.join(os.path.dirname(__file__), "../plots/")
         for name in os.listdir(plots_dir):
             if name.endswith(".py") and not name.startswith("_"):
@@ -249,11 +289,19 @@ class VisualizerController(object):
         self.registered = REGISTERED_PLOTS.values()
 
     def load_module(self, filename):
+        """ Loads a module by file name and
+        returns module object
+        """
         mod_name, ext = os.path.splitext(os.path.basename(filename))
         return imp.load_source(mod_name, filename)
 
     def on_layout_button_release(self, widget, event):
-        if event.button == 3:
+        """ Event handler for when export to pdf menu item is
+        activated in right click context menu for plots
+        
+        Returns true if event is handled here, false otherwise
+        """
+        if event.button == settings.EVENT_BUTTON_RIGHT_CLICK:
             export_pdf_item = gtk.MenuItem("Export to PDF...")
             export_pdf_item.connect("activate", self.on_export_pdf, widget)
             export_pdf_item.show()
@@ -265,6 +313,10 @@ class VisualizerController(object):
         return False
 
     def on_export_pdf(self, event_widget, widget, name=None):
+        """ Opens file to write pdf
+        Creates a pdf surface with file handler
+        Then draws to pdf surface and close file
+        """
         if not name:
             name = self.main_frame.window.get_title()
         filename = self.file_save(name + ".pdf")
@@ -282,18 +334,31 @@ class VisualizerController(object):
             cr.get_target().finish()
 
     def file_open(self, ext="", ext_name=""):
+        """ Calls _file_browse which opens a file browser with open button
+        
+        Returns result
+        """
         buttons = (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, 
                    gtk.STOCK_OPEN, gtk.RESPONSE_OK)
         return self._file_browse(gtk.FILE_CHOOSER_ACTION_OPEN, 
                                  buttons, "", ext, ext_name)
 
     def file_save(self, name="", ext="", ext_name=""):
+        """ Calls _file_browse which opens a file browser with save button
+        
+        Returns result
+        """
         buttons = (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, 
                    gtk.STOCK_SAVE, gtk.RESPONSE_OK)
         return self._file_browse(gtk.FILE_CHOOSER_ACTION_SAVE, 
                                  buttons, name, ext, ext_name)
 
     def _file_browse(self, action, buttons, name="", ext="", ext_name=""):
+        """ Creates a file chooser dialog
+        Use a * for filter
+        
+        Returns result of instantiating dialog
+        """
         dialog = gtk.FileChooserDialog(title="Select File", 
                                        action=action, buttons=buttons)
         dialog.set_current_folder(os.getcwd())
