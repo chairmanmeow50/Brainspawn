edc5e2ed7d3a2dd0aaa33b193a048f7754313e36
Tony Ren <chairmanmeow50@gmail.com>
2014-04-23 10:51:57 -0400

    documented brainspawn.py

--- brainspawn/brainspawn.py
+++ brainspawn/brainspawn.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Main app
+"""Laucher for Brainspawn visualization application.
 """
 
 from gi import pygtkcompat
@@ -9,16 +9,22 @@ import gtk
 import sys
 
 import warnings
+# Suppress some expected warnings
 warnings.filterwarnings("ignore", "tight_layout : falling back to Agg renderer")
 
 from simulator.sim_manager import SimManager
 from controllers.visualizer_controller import VisualizerController
+import settings
 
 def main():
+    """ Instantiates simulation manager. Checks for a model file in arguments,
+    if command line argument exists, attempt to load that model file directly.
+    Then instantiate visualizer controller.
+    """
     sim_manager = SimManager()
     if len(sys.argv) > 1:
         model_file = sys.argv[1]
-        if model_file.endswith(".py"):
+        if model_file.endswith(settings.PYTHON_FILE_EXTENSION):
             controller = VisualizerController(sim_manager, model_file)
     else:
         controller = VisualizerController(sim_manager)
