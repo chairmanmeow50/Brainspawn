c4058aa5a147b297e5a3e6e6af42806e07ae7a61
Tony Ren <chairmanmeow50@gmail.com>
2014-04-24 12:50:31 -0400

    menu_bar.py documented and PEP 8

--- brainspawn/views/components/menu_bar.py
+++ brainspawn/views/components/menu_bar.py
@@ -1,8 +1,19 @@
+""" Module for file menu bar in visualizer.
+"""
+
 import gtk
 
+
 class Menu_Bar(gtk.MenuBar):
+    """ Custom menu bar class called Menu_Bar.
+    """
 
     def __init__(self, main_frame, controller):
+        """ Initializes menu bar.
+
+        Adds file menu with items: open model and
+        export to pdf.
+        """
         self.controller = controller
         super(Menu_Bar, self).__init__()
 
@@ -14,18 +25,18 @@ class Menu_Bar(gtk.MenuBar):
         main_frame.window.add_accel_group(accel)
 
         open_menu_item = gtk.MenuItem("Open Model...")
-        open_menu_item.add_accelerator("activate", accel, ord('O'), 
+        open_menu_item.add_accelerator("activate", accel, ord('O'),
                                        gtk.gdk.CONTROL_MASK, gtk.ACCEL_VISIBLE)
         open_menu_item.connect("activate", controller.on_open_model)
         open_menu_item.show()
         file_submenu.append(open_menu_item)
 
         export_pdf_menu_item = gtk.MenuItem("Export to PDF...")
-        export_pdf_menu_item.connect('activate', controller.on_export_pdf, 
+        export_pdf_menu_item.connect('activate', controller.on_export_pdf,
                                      main_frame.window)
         export_pdf_menu_item.show()
         file_submenu.append(export_pdf_menu_item)
 
         file_menu.show()
 
-        self.append (file_menu)
+        self.append(file_menu)
