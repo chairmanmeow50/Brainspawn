db69dd62744c60418e47cfb6e69c7fc883309131
Tony Ren <chairmanmeow50@gmail.com>
2014-04-27 02:18:23 -0400

    spikes_cap.py now PEP 8

--- brainspawn/simulator/capabilities/spikes_cap.py
+++ brainspawn/simulator/capabilities/spikes_cap.py
@@ -1,49 +1,53 @@
-""" Spikes Capablity
+""" Spikes capability module
 Observes spiking activity in Ensembles and Neurons
 
-Not sure if nengo supports Neurons yet though
+Not sure if nengo supports Neurons yet
 """
 
 import nengo
 import numpy as np
 from capability import Capability
 
+
 class SpikesCap(Capability):
-    """Spikes Capability
+    """Spikes capability class.
     """
 
     @property
     def name(self):
-        """ Returns name of this capability, which is spikes
+        """ Returns name of this capability, which is spikes.
         """
         return "spikes"
 
     def supports_obj(self, obj):
-        """ Returns true if node is type Ensemble or Neurons
+        """ Returns true if node is type Ensemble or Neurons.
         """
-        return issubclass(obj.__class__, (nengo.Ensemble, nengo.nonlinearities.Neurons))
+        return issubclass(obj.__class__, (nengo.Ensemble,
+                                          nengo.nonlinearities.Neurons))
 
     def get_out_dimensions(self, obj):
-        """ Returns number of dimensions of node
+        """ Returns number of dimensions of node.
         """
-        if (issubclass(obj.__class__, (nengo.Ensemble, nengo.nonlinearities.Neurons))):
+        if (issubclass(obj.__class__, (nengo.Ensemble,
+                                       nengo.nonlinearities.Neurons))):
             return obj.n_neurons
         else:
             raise ValueError("output_cap does not support given object")
 
-
     def connect_node(self, node, obj):
-        """ Create connection between object and neurons, with identity transform
+        """ Create connection between object and neurons, with the identity
+        transformation.
 
-        See nengo.Ensemble.probe() for inspiration, also nengo.Connection
+        See nengo.Ensemble.probe() for inspiration, also nengo.Connection.
 
         TODO - specify filters
         """
         #probe = nengo.Probe(obj, "spikes")
         if (issubclass(obj.__class__, nengo.Ensemble)):
-            nengo.Connection(obj.neurons, node, filter=None, transform=np.eye(obj.n_neurons))
+            nengo.Connection(obj.neurons, node, filter=None,
+                             transform=np.eye(obj.n_neurons))
         elif (issubclass(obj.__class__, nengo.nonlinearities.Neurons)):
-            nengo.Connection(obj, node, filter=None, transform=np.eye(obj.n_neurons))
+            nengo.Connection(obj, node, filter=None,
+                             transform=np.eye(obj.n_neurons))
         else:
             raise ValueError("output_cap does not support given object")
-
