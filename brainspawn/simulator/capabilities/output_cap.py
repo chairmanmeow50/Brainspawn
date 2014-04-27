6d2f0e1a43f6cc9eff2e05e29bdf0166e3ad64a0
Tony Ren <chairmanmeow50@gmail.com>
2014-04-27 02:25:43 -0400

    output_cap.py now PEP 8

--- brainspawn/simulator/capabilities/output_cap.py
+++ brainspawn/simulator/capabilities/output_cap.py
@@ -1,30 +1,33 @@
-""" Output Capablity
-Basic output capability, creates simple connection between obj
-and observing node.
+""" Output capability module
 
-"decoded_output" for Ensembles
+Basic output capability, creates simple connection between an object
+and an observing node.
+
+"decoded_output" for Ensembles.
 """
 
 import nengo
 from capability import Capability
 
+
 class OutputCap(Capability):
-    """Output Capablity
+    """Output capability class.
     """
 
     @property
     def name(self):
-        """ Returns name of this capability, which is output
+        """ Returns name of this capability, which is 'output'.
         """
-        return "output" # 'decoded_ouput' for ensembles
+        return "output"  # 'decoded_ouput' for ensembles
 
     def supports_obj(self, obj):
-        """ Returns true if node is type Ensemble or Neurons
+        """ Returns true if node is type Ensemble or Neurons.
         """
-        return issubclass(obj.__class__, (nengo.Node, nengo.Ensemble, nengo.nonlinearities.Neurons))
+        return issubclass(obj.__class__, (nengo.Node, nengo.Ensemble,
+                                          nengo.nonlinearities.Neurons))
 
     def get_out_dimensions(self, obj):
-        """ Returns number of dimensions of node
+        """ Returns number of dimensions of node.
         """
         if (issubclass(obj.__class__, nengo.Node)):
             return obj.size_out
@@ -35,17 +38,16 @@ class OutputCap(Capability):
         else:
             raise ValueError("output_cap does not support given object")
 
-
     def connect_node(self, node, obj):
         """ Create 'simple' connection between object and node
-        For Ensembles, this gives us 'decoded_output'
+        For Ensembles, this gives us 'decoded_output'.
 
         TODO - specify filters
         """
         if (issubclass(obj.__class__, nengo.Node)):
             nengo.Connection(obj, node, filter=None)
-        elif (issubclass(obj.__class__, (nengo.Ensemble, nengo.nonlinearities.Neurons))):
+        elif (issubclass(obj.__class__, (nengo.Ensemble,
+                                         nengo.nonlinearities.Neurons))):
             nengo.Connection(obj, node, filter=0.005)
         else:
             raise ValueError("output_cap does not support given object")
-
