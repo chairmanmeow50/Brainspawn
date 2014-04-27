07e9af3abcbcb11c7de3cc63f20846a15bad96f1
Tony Ren <chairmanmeow50@gmail.com>
2014-04-27 02:27:46 -0400

    cap_factory.py now PEP 8

--- brainspawn/simulator/capabilities/cap_factory.py
+++ brainspawn/simulator/capabilities/cap_factory.py
@@ -1,22 +1,22 @@
-""" Factory for Caps
+""" Module for factory for capabilities.
 """
 
 from output_cap import OutputCap
 from spikes_cap import SpikesCap
 
+
 class CapFactory(object):
-    """Cap factory class
-    gets a list of all caps
+    """ Capability factory class.
+    Gets a list of all caps.
     """
 
     _caps = None
+
     @classmethod
     def get_caps(cls):
-        """Returns a list of all capabilities
-        *NB* - nengo doesn't support VoltageCap at the moment
+        """ Returns a list of all capabilities.
+        *NB* - nengo doesn't support VoltageCap at the moment.
         """
         if (not cls._caps):
             cls._caps = [OutputCap(), SpikesCap()]
         return cls._caps
-
-
