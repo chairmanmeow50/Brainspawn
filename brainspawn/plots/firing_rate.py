4355ca4abe36714441c0e9ce0e7847968d427fb4
Tony Ren <chairmanmeow50@gmail.com>
2014-04-28 01:33:10 -0400

    firing_rate.py now PEP8

--- brainspawn/plots/firing_rate.py
+++ brainspawn/plots/firing_rate.py
@@ -1,26 +1,34 @@
+""" Firing rate visualization. Ported from the Java version of Nengo.
+"""
+
 import numpy as np
 import math
 from matplotlib.patches import Rectangle
-from plots.plot import Plot 
+from plots.plot import Plot
 from plots.base_plot import registered_plot
+
 
 @registered_plot
 class Firing_Rate_Plot(Plot):
+    """ Firing rate plot. Extends from the Plot class.
+    """
 
     @staticmethod
     def plot_name():
+        """ Returns name of plot.
+        """
         return "Firing Rate"
 
     @staticmethod
     def supports_cap(cap):
+        """ Supports 'spikes' capability.
+        """
         return cap.name in ['spikes']
 
     def update(self, start_step, step_size, data):
-        """
-        paints a rectangle with a shade depending on
-        its previous values. if it fired on this tick
-        it will be white, if it fired in the previous
-        tick, it will be slightly grayer
+        """ Paints a rectangle with a shade depending on its previous values.
+        If it fired on this tick, it will be white. If it fired on the previous
+        tick, it will be slightly grayer.
         """
         if (len(data) == 0):
             self.clear()
@@ -46,6 +54,9 @@ class Firing_Rate_Plot(Plot):
                 self.rect_array[i][j].set_facecolor(color)
 
     def get_color(self, color_value):
+        """ Returns a colour based on a numeric value. Starting from 4, the
+        colour is white, and as the value goes down, becomes closer to black.
+        """
         if (color_value == 4):
             return "#FFFFFF"
         elif (color_value == 3):
@@ -58,27 +69,34 @@ class Firing_Rate_Plot(Plot):
             return "#000000"
 
     def clear(self):
-        """ sets all the grids to black """
+        """ Sets all the grid rectangles to black.
+        """
         for i in xrange(0, self.rows, 1):
             for j in xrange(0, self.rows, 1):
                 self.rect_array[i][j].set_facecolor("#000000")
 
     def draw_rects(self):
-        """ draws and initializes the initial rectangles """
+        """ Draws and initializes the initial rectangles.
+        """
         width = 1.0 / self.rows
         height = 1.0 / self.rows
 
         for x in xrange(0, self.rows, 1):
             for y in xrange(0, self.rows, 1):
-                rect = Rectangle((x/float(self.rows), y/float(self.rows)), width, height, facecolor="#000000")
+                rect = Rectangle((x/float(self.rows), y/float(self.rows)),
+                                 width, height, facecolor="#000000")
                 self.rect_array[x].append(rect)
                 self.rect_array_color[x] = np.zeros(self.rows)
                 self.axes.add_patch(rect)
 
     def __init__(self, main_controller, obj, cap):
+        """ Calls the parent constructor.
+        Creates x number of rows, where x is the square root of number of
+        dimensions (which means the number of neurons in an ensemble).
+        """
         super(Firing_Rate_Plot, self).__init__(main_controller, obj, cap)
 
-        self.axes = self.figure.add_subplot(111) # take first from list
+        self.axes = self.figure.add_subplot(111)  # take first from list
         self.axes.patch.set_alpha(0.0)
         self.axes.set_title(self.title)
 
