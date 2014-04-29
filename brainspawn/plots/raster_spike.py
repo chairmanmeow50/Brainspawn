9f51b393eceea44e0fd1235cdd44336301df9d4b
Tony Ren <chairmanmeow50@gmail.com>
2014-04-29 02:54:27 -0400

    raster_spike.py now PEP8

--- brainspawn/plots/raster_spike.py
+++ brainspawn/plots/raster_spike.py
@@ -1,13 +1,21 @@
+""" Module for raster spike visualization. Ported from old Java nengo.
+"""
+
 from plots.base_plot import registered_plot
 from plots.plot import Plot
 import settings
 
+
 @registered_plot
 class RasterSpike(Plot):
+    """ Class for raster spike visualization. Extends from Plot.
+    """
 
     WINDOW_SIZE = 100
 
     def __init__(self, main_controller, obj, cap):
+        """ Initializes class, sets title, and labels.
+        """
         super(RasterSpike, self).__init__(main_controller, obj, cap)
 
         self.axes = self.figure.add_subplot(111)
@@ -16,19 +24,26 @@ class RasterSpike(Plot):
         self.axes.set_ylabel("Neurons")
         self.axes.set_xlabel("Time (s)")
         #TODO: The 1.0 here shouldn't be hardcoded, as with all plots
-        self._initial_end_time = 1.0 * self.WINDOW_SIZE / settings.MAX_WINDOW_SIZE
+        self._initial_end_time = 1.0 * \
+            self.WINDOW_SIZE / settings.MAX_WINDOW_SIZE
         self.axes.set_xlim([0, self._initial_end_time])
         self._image = None
 
     @staticmethod
     def plot_name():
+        """ Returns name of plot.
+        """
         return "Raster Spike"
 
     @staticmethod
     def supports_cap(cap):
+        """ Supports the 'spikes' capability.
+        """
         return cap.name in ["spikes"]
 
     def update(self, start_step, step_size, spikes):
+        """ Plots the spikes with imshow (which produces a rasterized image).
+        """
         if self._image:
             self._image.remove()
             self._image = None
@@ -38,8 +53,11 @@ class RasterSpike(Plot):
         end_step = start_step + spikes.shape[0]
         start_time = (end_step - spikes_view.shape[0]) * step_size
         end_time = end_step * step_size
-        self._image = self.axes.imshow(  # interpolation="none" is not supported on cairo
-                spikes_view.T, cmap="binary", aspect="auto", interpolation="nearest", origin="lower",
-                extent=(start_time, end_time, 0, spikes_view.shape[1]))
-        
-        self.set_default_xlim(max(end_time, self._initial_end_time), self._initial_end_time)
+        self._image = self.axes.imshow(
+            # interpolation="none" is not supported on cairo
+            spikes_view.T, cmap="binary", aspect="auto",
+            interpolation="nearest", origin="lower",
+            extent=(start_time, end_time, 0, spikes_view.shape[1]))
+
+        self.set_default_xlim(max(end_time, self._initial_end_time),
+                              self._initial_end_time)
